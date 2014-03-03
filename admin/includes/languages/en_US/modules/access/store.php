d9ad995effde7a90b57c7312c918a32e692cf1d8
tocjack <jackyin@tomatocart.com>
2014-03-03 16:33:00 +0800

    [#207]Add the store module menu into the start menu of admin panel

---
+++ admin/includes/languages/en_US/modules/access/store.php
@@ -0,0 +1,13 @@
+# $Id: store.php $
+# TomatoCart Open Source Shopping Cart Solutions
+# http://www.tomatocart.com
+#
+# Copyright (c) 2009 TomatoCart;
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License v2 (1991)
+# as published by the Free Software Foundation.
+
+access_store_title = Store
+
+
---
+++ admin/includes/modules/access/store.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $Id: store.php $
+  TomatoCart Open Source Shopping Cart Solutions
+  http://www.tomatocart.com
+
+  Copyright (c) 2009 TomatoCart;  Copyright (c) 2007 osCommerce
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License v2 (1991)
+  as published by the Free Software Foundation.
+*/
+
+  class osC_Access_store extends osC_Access {
+    var $_module = 'store',
+        $_group = 'definitions',
+        $_icon = 'photo_add.png',
+        $_title,
+        $_sort_order = 0;
+
+    function osC_Access_store() {
+      global $osC_Language;
+
+      $this->_title = $osC_Language->get('access_store_title');
+    }
+  }
+?>
